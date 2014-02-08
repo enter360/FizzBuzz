387ef0a9dada5c2aec1c5e63ab46bc77f83b525e
Corbin Black <enter360@gmail.com>
2014-02-08 15:54:42 -0600

    my commit message

---
+++ FizzBuzz/main.cpp
@@ -0,0 +1,83 @@
+//
+//  main.cpp
+//  FizzBuzz
+//
+//  Created by Corbin Black on 2/3/14.
+//  Copyright (c) 2014 Corbin Black. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+void FizzBuzz(int count){
+
+    for (int i =1; i <= count; i++) {
+            // Testing if the number is a multiple of 3 & 5
+        if (i%5 == 0 && i%3 == 0) {
+            
+            cout<<"FizzBuzz"<<endl;
+            
+            //Testing if the number is a multiple of 3
+        }else if (i%3 ==0){
+            
+            cout<<"Fizz"<<endl;
+            
+            //Testing if the number is a multiple of 5
+        }else if (i%5 == 0){
+            cout<<"Buzz"<<endl;
+        }else{
+            cout<<i<<endl;
+        }
+        
+    }
+    
+    
+    cout<<"Done Thank you"<<endl;
+}
+
+
+bool ProperReply( char reply){
+    
+    //cout<<"Proper Reply"<<endl;
+    
+    if (reply == 'Y' || reply =='y' ) {
+        return true;
+       // cout<<"Returning true"<<endl;
+    }else if ( reply == 'N' || reply == 'n') {
+        return false;
+      //  cout<<"Returning False"<<endl;
+    }
+    //cout<<"exiting Proper"<<endl;
+    return false;
+}
+
+int main(int argc, const char * argv[])
+{
+    
+    int limit;
+    char reply;
+    bool cont= false;
+    
+    
+    do {
+        
+        cout<<"What do you want to FizzBuzz to ? "<<endl;
+        cin>>limit;
+        
+        FizzBuzz(limit);
+        cout<<"Do you want to FizzBuzz again(Y/N) ? "<<endl;
+        cin>>reply;
+        
+        cont = ProperReply(reply);
+        
+        
+    } while (cont == true);
+    
+    cout<<"thank you for playing"<<endl;
+    
+    return 0;
+}
+
